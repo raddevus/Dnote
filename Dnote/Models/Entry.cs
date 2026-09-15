e5f472ed70d5b5a36aee3f4bdf4574c670da9f60
raddevus <1radeutsch1@gmail.com>
2026-09-15 10:27:38 -0400

    Added new Entry class which will manage the daily entry file and tie it to a date.

---
+++ Dnote/Models/Entry.cs
@@ -0,0 +1,49 @@
+using System;
+using System.IO;
+
+public class Entry{
+    public String Data{get;set;}
+    public DateTime Created{get;set;}
+
+    public String EntryFolder{get;set;}
+
+    public String EntryFile{get;set;}
+    public Entry(String date, String data)
+    {
+        try{
+            Created = DateTime.Parse(date);
+        }
+        catch{
+            Console.WriteLine("couldn't parse entry date");
+        }
+        EntryFolder = Created.ToString("yyyy-MM");
+        EntryFile = $"{Created.ToString("yyyy-MM-dd")}.txt";
+        Data = data;
+    }
+
+    public bool Save(){
+        try{
+            Directory.CreateDirectory(EntryFolder);
+            var targetFile = Path.Combine(EntryFolder,EntryFile);
+            File.Delete(targetFile);
+            File.AppendAllText(targetFile,Data);
+        }
+        catch{
+            return false;
+        }
+        return true;
+    }
+
+    public String Read(){
+        try{
+            var currentDateFile = Path.Combine(EntryFolder,EntryFile);
+            if (File.Exists(currentDateFile)){
+                return File.ReadAllText(currentDateFile);
+            }
+            Console.WriteLine($"An entry doesn't exist yet for {Path.Combine(EntryFolder,EntryFile)} ");
+            return String.Empty;
+        }
+        catch{Console.WriteLine($"couldn't read data from {Path.Combine(EntryFolder,EntryFile)} ");
+        return String.Empty;}
+    }
+}
